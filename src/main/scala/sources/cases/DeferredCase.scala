bd580041adcb06b271e67f2c2d4b5a03df4cc853
予一兮 <qingluanxi@gmail.com>
2024-08-25 02:06:20 +0800

    add a defer case

--- src/main/scala/sources/cases/DeferredCase.scala
+++ src/main/scala/sources/cases/DeferredCase.scala
@@ -1,25 +1,40 @@
 package sources.cases
 
+import cats.effect.unsafe.implicits.global
 import cats.effect.{Deferred, IO}
-import cats.implicits.catsSyntaxParallelSequence1
+import cats.implicits.{catsSyntaxApplyOps, catsSyntaxParallelSequence1}
 
 object DeferredCase extends App {
 
-    def start(d:Deferred[IO,Int]) = {
-      val attempt: Int => IO[Unit] = n =>d.complete(n).void
-      List(
-        IO.race(attempt(1), attempt(2)),
-        d.get.flatMap { n => IO(println(s"Result: $n")) }
-      ).parSequence.void
-    }
+  def start(d: Deferred[IO, Int]) = {
+    val attempt: Int => IO[Unit] = n => d.complete(n).void
+    List(
+      IO.race(attempt(1), attempt(2)),
+      d.get.flatMap { n => IO(println(s"Result: $n")) }
+    ).parSequence.void
+  }
 
   val program = {
-    for{
-      d <- Deferred[IO,Int]
+    for {
+      d <- Deferred[IO, Int]
       _ <- start(d)
-    }yield ()
+    } yield ()
   }
 
   val run: IO[Unit] = program
+
+}
+
+
+object DeferredCase1 extends App {
+
+
+  IO.deferred[Deferred[IO, Int]].flatMap { outDefer =>
+    IO.deferred[Int].flatMap { innerDefer =>
+      //这里outDefer其实容纳了innerDefer的complete数据
+      outDefer.complete(innerDefer) *> innerDefer.complete(10)
+    } *> outDefer.get.flatMap(_.tryGet).flatMap(a=>IO.println(s"out data $a"))  // out data Some(10)
+  }.unsafeRunSync()
+
 
 }
