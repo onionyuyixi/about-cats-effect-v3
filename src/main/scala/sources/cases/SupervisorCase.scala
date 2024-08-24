3e10b0ffb8a0fd5ece6a0d5cca63bd682f0c4d99
予一兮 <qingluanxi@gmail.com>
2024-08-24 22:09:42 +0800

    update SupervisorCase

--- src/main/scala/sources/cases/SupervisorCase.scala
+++ src/main/scala/sources/cases/SupervisorCase.scala
@@ -9,12 +9,12 @@ import scala.concurrent.duration.DurationInt
 
 object SupervisorCase extends IOApp.Simple {
 
-  val ioa: IO[String] = IO.sleep(1.seconds) *> IO(s"ioa ${Thread.currentThread().getName}")
+  val ioa: IO[String] = IO.sleep(1.seconds) >> IO(s"ioa ${Thread.currentThread().getName}")
 
   override def run: IO[Unit] = {
     val io: IO[Unit] = Supervisor[IO].use {
       supervisor => {
-        // 给ioa生成的新fiber 绑定一个指定的生命周期 Supervisor也是一个Resource
+        // 给ioa生成的新fiber 绑定一个指定的生命周期
         val iob = supervisor.supervise(ioa).flatMap(f => {
           f.join.flatMap {
             case Succeeded(fa) => fa.map(a => a + s" in supervisor ${Thread.currentThread().getName}")
@@ -22,11 +22,14 @@ object SupervisorCase extends IOApp.Simple {
             case Outcome.Canceled() => IO("取消")
           }
         })
-        (iob.map((b: String) => println(s"over!!! the result is ioa-->$b ")) ->
-          IO.sleep(10000.milliseconds) *> IO.raiseError(new Exception("err")))
-          .parSequence_
+
+        (iob.flatMap((b: String) => IO.println(s"over!!! the result is ioa-->$b ")) *>
+          IO.sleep(1000.milliseconds).start *>
+          supervisor.supervise(IO.println(s"after 1s ${}"))
+          ).void
       }
     }
-    (ioa->io).parSequence_
+
+    (ioa -> io).flatMapN((str, _) => IO.println(s"ioa result is $str")).void
   }
 }
